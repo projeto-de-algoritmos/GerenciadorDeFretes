fd6ab5d4b171033930b04a8aed1d14919fa4325d
VictorJorgeFGA <victor.eng.unb@gmail.com>
2021-04-25 11:48:09 -0300

    Add DynamicText class

---
+++ GerenciadorDeFretes/inc/DynamicText.hpp
@@ -0,0 +1,46 @@
+#ifndef _DYNAMIC_TEXT_HPP_
+#define _DYNAMIC_TEXT_HPP_
+
+#include "VisualComponent.hpp"
+#include "SolidText.hpp"
+#include "Assets.hpp"
+
+#include <string>
+#include <vector>
+#include <SDL2/SDL.h>
+
+class DynamicText : public VisualComponent
+{
+public:
+    static DynamicText * newDynamicText(const std::string & text,
+                                        const std::string & font_name = Assets::DYNAMICTEXT_FONT_NAME,
+                                        uint16_t font_size = Assets::DYNAMICTEXT_FONT_SIZE,
+                                        const SDL_Color & font_color = Assets::DYNAMICTEXT_FONT_COLOR);
+
+    void setText(const std::string & text);
+
+    uint16_t getWidth() const noexcept;
+    uint16_t getHeight() const noexcept;
+
+    void hide();
+    void show();
+
+private:
+    DynamicText(const std::string & font_name = "",
+                uint16_t font_size = 0,
+                const SDL_Color & font_color = {0xff, 0xff, 0xff, 0xff});
+
+    DynamicText(DynamicText &);
+    virtual ~DynamicText();
+
+    std::string _text;
+    SDL_Color _font_color;
+    uint16_t _font_size;
+    std::string _font_name;
+    std::vector<SolidText *> _chars;
+
+protected:
+
+};
+
+#endif
---
+++ GerenciadorDeFretes/src/DynamicText.cpp
@@ -0,0 +1,98 @@
+#include "DynamicText.hpp"
+#include "AssetsManager.hpp"
+
+#include <cstdint>
+
+DynamicText * DynamicText::newDynamicText(const std::string & text,
+                                          const std::string & font_name,
+                                          uint16_t font_size,
+                                          const SDL_Color & font_color)
+{
+    DynamicText * dt = new DynamicText(font_name, font_size, font_color);
+    dt->setText(text);
+
+    return dt;
+}
+
+void DynamicText::setText(const std::string & text)
+{
+    if (text == _text)
+        return;
+
+    _text = text;
+
+    while (_chars.size() < text.size()) {
+        _chars.push_back(SolidText::newSolidText("_", _font_name, _font_size, _font_color));
+
+        if (_chars.size() == 1)
+            _chars.back()->setParent(this);
+        else
+            _chars.back()->setParent(_chars[_chars.size() - 2]);
+
+        _chars.back()->setRelativeY(0);
+    }
+
+    AssetsManager * am = AssetsManager::getInstance();
+    for (int32_t i = 0; i < (int32_t) text.size(); i++) {
+        _chars[i]->setTexture(am->getTextTexture(_font_name, _font_size, std::string(1, text[i]), _font_color));
+        _chars[i]->setRelativeX(_chars[i]->getParent()->getWidth());
+        _chars[i]->show();
+    }
+
+    for (int32_t i = text.size(); i < (int32_t) _chars.size(); i++)
+        _chars[i]->hide();
+}
+
+uint16_t DynamicText::getWidth() const noexcept
+{
+    uint16_t total_width = 0;
+    for (auto child : _chars) {
+        if (child->isHide())
+            break;
+        total_width += child->getWidth();
+    }
+    return total_width;
+}
+
+uint16_t DynamicText::getHeight() const noexcept
+{
+    uint16_t total_height = 0;
+    for (auto child : _chars) {
+        if (child->isHide())
+            break;
+        total_height += child->getHeight();
+    }
+    return total_height;
+}
+
+void DynamicText::hide()
+{
+    for (int32_t i = 0; i < (int32_t) _text.size(); i++)
+        _chars[i]->hide();
+}
+
+void DynamicText::show()
+{
+    for (int32_t i = 0; i < (int32_t) _text.size(); i++)
+        _chars[i]->show();
+}
+
+DynamicText::DynamicText(const std::string & font_name, uint16_t font_size, const SDL_Color & font_color):
+VisualComponent(0,0),
+_font_color(font_color),
+_font_size(font_size),
+_font_name(font_name)
+{
+
+}
+
+DynamicText::DynamicText(DynamicText & dt):
+VisualComponent(0,0)
+{
+
+}
+
+DynamicText::~DynamicText()
+{
+
+}
