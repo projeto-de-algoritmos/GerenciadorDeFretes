eed0d17a2fbb9498bd59a55d2f6ceac36907670f
VictorJorgeFGA <victor.eng.unb@gmail.com>
2021-04-25 11:44:59 -0300

    Add VisualComponent class

---
+++ GerenciadorDeFretes/inc/VisualComponent.hpp
@@ -0,0 +1,93 @@
+#ifndef _VISUAL_COMPONENT_HPP_
+#define _VISUAL_COMPONENT_HPP_
+
+#include "Graphics.hpp"
+#include <SDL2/SDL.h>
+
+#include <vector>
+#include <string>
+
+class VisualComponent
+{
+public:
+    static void setVerboseMode();
+
+    static void startUp();
+    static void shutDown();
+    static void drawComponents();
+    static VisualComponent * getScreenObject();
+
+    void setParent(VisualComponent * new_parent);
+    VisualComponent * getParent() noexcept;
+
+    bool isAChild(VisualComponent * component) const noexcept;
+
+    std::vector<VisualComponent *> getChildren() const noexcept;
+    int32_t countChildren() const noexcept;
+
+    void hide() noexcept;
+    void show() noexcept;
+    bool isHide() const noexcept;
+
+    void setTexture(SDL_Texture * texture) noexcept;
+    SDL_Texture * getTexture() noexcept;
+
+    SDL_Rect getGlobalBody() const noexcept;
+    SDL_Rect getRelativeBody() const noexcept;
+
+    int32_t getGlobalX() const noexcept;
+    int32_t getRelativeX() const noexcept;
+    void setGlobalX(int32_t x) noexcept;
+    void setRelativeX(int32_t x) noexcept;
+
+    int32_t getGlobalY() const noexcept;
+    int32_t getRelativeY() const noexcept;
+    void setGlobalY(int32_t y) noexcept;
+    void setRelativeY(int32_t y) noexcept;
+
+    uint16_t getWidth() const noexcept;
+    void setWidth(uint16_t width) noexcept;
+
+    uint16_t getHeight() const noexcept;
+    void setHeight(uint16_t height) noexcept;
+
+    double getRotationAngle() const noexcept;
+    void setRotationAngle(double rotation_angle) noexcept;
+    void rotateClockwise(double amount) noexcept;
+
+    SDL_Color getColor() const noexcept;
+    void setColor(SDL_Color color) noexcept;
+
+    void addChild(VisualComponent * child);
+    void removeChild(VisualComponent * child);
+    
+    void draw();
+
+private:
+    static VisualComponent * SCREEN;
+    static bool VERBOSE;
+    static const std::string VERBOSE_MSG;
+    static const std::string ERROR_MSG;
+    static Graphics * graphics;
+    static void throwException(const std::string & msg);
+
+    VisualComponent();
+    
+    std::vector<VisualComponent *> _children;
+    VisualComponent * _parent;
+    SDL_Texture * _texture;
+    double _rotation_angle;
+    bool _is_hide;
+    SDL_Rect _body;
+    SDL_Color _color;
+
+protected:
+
+    VisualComponent(uint16_t width, uint16_t height,
+                    SDL_Color color = {0xFF, 0xFF, 0xFF, 0xFF},
+                    SDL_Texture * texture = nullptr);
+
+    virtual ~VisualComponent();
+};
+
+#endif
---
+++ GerenciadorDeFretes/src/VisualComponent.cpp
@@ -0,0 +1,317 @@
+#include "VisualComponent.hpp"
+#include "Graphics.hpp"
+
+#include <stdexcept>
+#include <iostream>
+
+VisualComponent * VisualComponent::SCREEN = nullptr;
+bool VisualComponent::VERBOSE = false;
+const std::string VisualComponent::VERBOSE_MSG = "\033[0;33m(VERBOSE) VisualComponent:\t\033[0m";
+const std::string VisualComponent::ERROR_MSG = "\033[0;31m(ERROR) VisualComponent:\t\033[0m";
+Graphics * VisualComponent::graphics = nullptr;
+
+void VisualComponent::setVerboseMode()
+{
+    VERBOSE = true;
+}
+
+void VisualComponent::startUp()
+{
+    if (SCREEN == nullptr) {
+        graphics = Graphics::getInstance();
+        SCREEN = new VisualComponent();
+        SCREEN->setWidth(graphics->getWindowWidth());
+        SCREEN->setHeight(graphics->getWindowHeight());
+
+        if (VERBOSE) std::cout << VERBOSE_MSG << "Starting up" << std::endl;
+    }
+    else
+        throwException("Attempt to initialize VisualComponent more than once");
+}
+
+void VisualComponent::shutDown()
+{
+    if (SCREEN == nullptr)
+        throwException("Attempt to shut down VisualComponent before initialization");
+    
+    delete SCREEN;
+    SCREEN = nullptr;
+    graphics = nullptr;
+
+    if (VERBOSE) std::cout << VERBOSE_MSG + "Shutting down\n" << std::endl; 
+}
+
+void VisualComponent::drawComponents()
+{
+    if (SCREEN == nullptr)
+        throwException("Attempt to draw VisualComponent objects before initialization");
+    
+    graphics->clearScreen();
+    SCREEN->draw();
+    graphics->updateScreen();
+}
+
+VisualComponent * VisualComponent::getScreenObject()
+{
+    if (SCREEN == nullptr)
+        throwException("Attempt to get VisualComponent SCREEN object before initialization");
+    
+    return SCREEN;
+}
+
+void VisualComponent::setParent(VisualComponent * new_parent)
+{
+    if (new_parent == _parent)
+        return;
+    else if (new_parent == nullptr || new_parent == NULL)
+        throwException("Attempt to set VisualComponent object parent as nullptr");
+    else if (new_parent == this)
+        throwException("Attempt to set itself as a parent");
+    else if (isAChild(new_parent))
+        throwException("Attempt to set a child as a parent");
+
+    if (_parent != nullptr)
+        _parent->removeChild(this);
+
+    new_parent->addChild(this);
+    _parent = new_parent;
+}
+
+VisualComponent * VisualComponent::getParent() noexcept
+{
+    return _parent;
+}
+
+bool VisualComponent::isAChild(VisualComponent * component) const noexcept
+{
+    for (auto e : _children) {
+        if (e == component)
+            return true;
+    }
+    return false;
+}
+
+std::vector<VisualComponent *> VisualComponent::getChildren() const noexcept
+{
+    return _children;
+}
+
+int32_t VisualComponent::countChildren() const noexcept
+{
+    return (int32_t) _children.size();
+}
+
+void VisualComponent::hide() noexcept
+{
+    _is_hide = true;
+}
+
+void VisualComponent::show() noexcept
+{
+    _is_hide = false;
+}
+
+bool VisualComponent::isHide() const noexcept
+{
+    return _is_hide;
+}
+
+void VisualComponent::setTexture(SDL_Texture * texture) noexcept
+{
+    _texture = texture;
+}
+
+SDL_Texture * VisualComponent::getTexture() noexcept
+{
+    return _texture;
+}
+
+SDL_Rect VisualComponent::getGlobalBody() const noexcept
+{
+    if (_parent == nullptr)
+        return _body;
+   
+    return {getGlobalX(), getGlobalY(), getWidth(), getHeight()};
+}
+
+SDL_Rect VisualComponent::getRelativeBody() const noexcept
+{
+    return _body;
+}
+
+int32_t VisualComponent::getGlobalX() const noexcept
+{
+    return (_parent == nullptr ? _body.x : _body.x + _parent->getGlobalX());
+}
+
+int32_t VisualComponent::getRelativeX() const noexcept
+{
+    return _body.x;
+}
+
+void VisualComponent::setGlobalX(int32_t x) noexcept
+{
+    if (_parent == nullptr)
+        _body.x = x;
+    else
+        _body.x = x - _parent->getGlobalX();
+}
+
+void VisualComponent::setRelativeX(int32_t x) noexcept
+{
+    _body.x = x;
+}
+
+int32_t VisualComponent::getGlobalY() const noexcept
+{
+    return _parent == nullptr ? _body.y : _body.y + _parent->getGlobalY();
+}
+
+int32_t VisualComponent::getRelativeY() const noexcept
+{
+    return _body.y;
+}
+
+void VisualComponent::setGlobalY(int32_t y) noexcept
+{
+    if (_parent == nullptr)
+        _body.y = y;
+    else
+        _body.y = y - _parent->getGlobalY();
+}
+
+void VisualComponent::setRelativeY(int32_t y) noexcept
+{
+    _body.y = y;
+}
+
+uint16_t VisualComponent::getWidth() const noexcept
+{
+    return _body.w;
+}
+
+void VisualComponent::setWidth(uint16_t width) noexcept
+{
+    _body.w = width;
+}
+
+uint16_t VisualComponent::getHeight() const noexcept
+{
+    return _body.h;
+}
+
+void VisualComponent::setHeight(uint16_t height) noexcept
+{
+    _body.h = height;
+}
+
+double VisualComponent::getRotationAngle() const noexcept
+{
+    return _rotation_angle;
+}
+
+void VisualComponent::setRotationAngle(double rotation_angle) noexcept
+{
+    _rotation_angle = rotation_angle;
+}
+
+void VisualComponent::rotateClockwise(double amount) noexcept
+{
+    setRotationAngle(getRotationAngle() + amount);
+    for (auto child : _children)
+        child->rotateClockwise(amount);
+}
+
+SDL_Color VisualComponent::getColor() const noexcept
+{
+    return _color;
+}
+
+void VisualComponent::setColor(SDL_Color color) noexcept
+{
+    _color = color;
+}
+
+void VisualComponent::addChild(VisualComponent * child)
+{
+    if (child == nullptr)
+        throwException("Attempt to push nullptr as a child");
+    else if (child == this)
+        throwException("Attempt to push itself as a child");
+    else if (child == getParent())
+        throwException("Attempt to push parent as a child");
+
+    for (auto e : _children) {
+        if (e == child)
+            return;
+    }
+    _children.push_back(child);
+}
+
+void VisualComponent::removeChild(VisualComponent * child)
+{
+    auto iter = _children.begin();
+    for ( ; iter != _children.end(); iter++)
+        if (*iter == child) break;
+    if (iter != _children.end())
+        _children.erase(iter);
+}
+
+void VisualComponent::draw()
+{
+    if (_texture != nullptr && !_is_hide)
+        graphics->drawTexture(_texture, getGlobalBody(), getRotationAngle());
+    else if (_texture == nullptr && !_is_hide)
+        graphics->drawRectangle(getGlobalBody(), _color);
+    
+    for (auto child : _children)
+        child->draw();
+}
+
+void VisualComponent::throwException(const std::string & msg)
+{
+    std::cerr << ERROR_MSG << msg << std::endl;
+    throw std::runtime_error("VisualComponent exception");
+}
+
+VisualComponent::VisualComponent():
+_children(),
+_parent(nullptr),
+_texture(nullptr),
+_rotation_angle(0.0),
+_is_hide(false),
+_body({0,0,0,0}),
+_color({0xff, 0xff, 0xff, 0xff})
+{
+
+}
+
+VisualComponent::VisualComponent(uint16_t width, uint16_t height, SDL_Color color, SDL_Texture * texture):
+_children(),
+_parent(nullptr),
+_texture(texture),
+_rotation_angle(0.0),
+_is_hide(false),
+_body({0, 0, width, height}),
+_color(color)
+{
+    setParent(getScreenObject());
+}
+
+VisualComponent::~VisualComponent()
+{
+    for (auto child : _children) {
+        
+        if (VERBOSE) {
+            std::cout << VERBOSE_MSG + "Deleting this component:" << std::endl;
+
+            graphics->clearScreen();
+            if (child->getTexture() != nullptr)
+                graphics->drawTexture(child->getTexture(), child->getGlobalBody());
+            graphics->updateScreen();
+            SDL_Delay(500);
+        }
+
+        delete child;
+    }
+}
